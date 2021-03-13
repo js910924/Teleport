fb7f7d40003a5f8437d27bb0da06139b8d35102d
Jason Chen <jason.chen@titansoft.com.sg>
2021-03-13 15:53:54 +0800

    upgrade test project to Dotnet 5

---
+++ Teleport.UnitTests/Controller/PortfolioControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Teleport.Controllers;
+using Teleport.Repository;
+
+namespace Teleport.UnitTests.Controller
+{
+    [TestFixture]
+    public class PortfolioControllerTests
+    {
+        [Test]
+        public void should_get_all_history_transactions_when_call_Position()
+        {
+            var stockTransactionRepo = NSubstitute.Substitute.For<IStockTransactionRepo>();
+
+            var controller = new PortfolioController(stockTransactionRepo);
+        }
+        
+    }
+}
